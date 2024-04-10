15134d40944cd3370958c78110dbc43df550ec9d
mañana <maana@A23-23>
2024-04-10 13:28:31 +0200

    segundo mensaje

--- java1/src/paquete1/principal.java
+++ java1/src/paquete1/principal.java
@@ -2,6 +2,7 @@ package paquete1;
 
 public class principal {
 	public static void main(String[] args) { System.out.println("Estamos haciendo control de versiones con Git"); 
+	System.out.println("cambio para ver el historial");
 	} 
-	}
+}
 
